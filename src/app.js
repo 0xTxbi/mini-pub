eeb90b82bf3429d775b668033bf7fed7155168e0
TechieJossy <techieJossy@users.noreply.github.com>
2020-11-06 11:45:29 +0100

    Feature: Import custom HTTP Library and obtain data from dummy API

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
+// Import the HTTP library
+import { http } from './http';
+
+
+// Define function to obtain posts
+const getPosts = () => {
+
+	http.get('http://localhost:3000/posts')
+	.then(data => console.log(data))
+	.catch(err => console.log(err));
+
+};
+
+
+// GET Posts when DOM loads
+document.addEventListener('DOMContentLoaded', getPosts);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Display current date in the footer section
 
 // Obtain intended display area
@@ -7,4 +36,4 @@ const currDateSel = document.querySelector('.current-date');
 const currDate = new Date().getFullYear();
 
 // Display in the selcted display area
-currDateSel.innerHTML = currDate.toString();
+currDateSel.textContent = currDate.toString();
